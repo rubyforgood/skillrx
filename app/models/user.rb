c18e315b0dae04fac276d3243b740a1c5fbb23f5
Olivia Bonnal <oliviabonnal@gmail.com>
2025-02-03 10:51:31 +0000

    Add Rails 8 authentication
    "Remember me" seems to be already handled by
    Authentication#start_new_session_for(user), which creates a permanent
    cookie for the user.

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :sessions, dependent: :destroy
+
+  normalizes :email, with: ->(e) { e.strip.downcase }
+
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root "home#index"
+  get "home/index", as: :home
+  resource :session
+  resources :passwords, param: :token
+  resources :users, only: :create
+  resource :registration, only: %i[new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
