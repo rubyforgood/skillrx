967e8098e251dcdc2349d6019c0a8e921bec2c9b
Dalyl Feddal <dalylfeddal@MacBook-Air-de-Dalyl.local>
2025-02-03 15:45:37 +0100

    add design for password, add route for reset password, add layout for password, condition in layout mazer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resource :session
-  resources :passwords, param: :token
+  resources :passwords, except: [:new], param: :token
   resources :users, only: :create
   resource :registration, only: %i[new create]
   resources :regions
@@ -12,13 +12,12 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "home/index", as: :home
-
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "connexion", to: "sessions#new"
+  get 'reset_password', to:"passwords#new"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "connexion", to: "sessions#new"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
