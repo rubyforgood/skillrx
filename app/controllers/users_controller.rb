f3f9cb5e89dbf5fc7e21e0e61ce522e580e2decc
Dalyl F <dalylf97@gmail.com>
2025-02-03 16:57:09 +0100

    view user and method crud, not finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  layout "mazer"
   def index
+    @users = User.all
+    #users = User.all
+  
   end
+
+  def create
+  end
+  def new
+  end
+  def edit
+  end
+  def destroy
+  end
+
 end
