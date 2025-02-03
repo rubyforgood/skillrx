d4933a93376ff7227ab08067ff26e870b0d608d8
Dalyl Feddal <dalylfeddal@MacBook-Air-de-Dalyl.local>
2025-02-03 15:26:09 +0100

    condition in file mazer for login page, new route for connexion, design for session/new, session controler : layout -> mazer

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "connexion", to: "sessions#new"
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  resources :users
   # Defines the root path route ("/")
   root "home#index"
 end
