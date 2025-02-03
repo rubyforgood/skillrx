14e673b9156a22bda12556b7bb80cb087775cf7f
Hernan Maguina <hernan.maguina@gmail.com>
2025-02-03 14:03:23 +0100

    Add region model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_03_094127) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_03_125727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "regions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
