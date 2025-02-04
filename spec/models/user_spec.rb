4a0d679eab4dbbabd0ec9cdcd31a62b8a7747f50
Sean Marcia <smarcia@gmail.com>
2025-02-04 17:10:00 +0100

    Demo of a failing lint and spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,16 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  describe "passing validations" do
   subject { create(:user) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
   it { should validate_uniqueness_of(:email).ignoring_case_sensitivity  }
+  end
+  describe "failing validations" do
+    it "fails if email is not present" do
+      user = User.new(email: nil, password: "password"       )
+                      expect(user).to_ be_valid
+    end
+  end
 end
