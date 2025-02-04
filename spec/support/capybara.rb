c226fd571d66d00db43088c8cd167bc95d2be0ec
Olivia Bonnal <oliviabonnal@gmail.com>
2025-02-04 20:47:43 +0000

    Fix "Add New User" button on user index
    This fixes the url and applies the same styling as on other similar pages.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,6 +6,12 @@ Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
+module CapybaraPage
+  def page
+    Capybara.string(response.body)
+  end
+end
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
@@ -14,4 +20,6 @@ RSpec.configure do |config|
   config.before(:each, :debug, type: :system) do
     driven_by :selenium_chrome
   end
+
+  config.include CapybaraPage, type: :request
 end
