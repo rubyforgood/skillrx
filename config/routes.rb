8f9eb72339184bc4c1427da826a55f42e965ab2f
dmitrytrager <dmitry.trager@gmail.com>
2025-02-04 08:39:12 +0100

    update authentication helpers for tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :regions
-  mount Maily::Engine, at: "/maily"
+  (mount Maily::Engine, at: "/maily") if defined?(Maily)
   root "home#index"
   get "home/index", as: :home
   resource :session
--- lib/maily_hooks.rb
+++ lib/maily_hooks.rb
@@ -1,5 +1,7 @@
-user = ""
+if defined?(Maily)
+  user = ""
 
-Maily.hooks_for("PasswordsMailer") do |mailer|
-  mailer.register_hook(:reset, user)
+  Maily.hooks_for("PasswordsMailer") do |mailer|
+    mailer.register_hook(:reset, user)
+  end
 end
