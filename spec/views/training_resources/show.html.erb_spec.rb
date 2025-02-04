fd1423e13bf768b4ec909bd7848a7f2278e195e7
Dalyl F <dalylf97@gmail.com>
2025-02-04 16:53:40 +0100

    fix linter

--- spec/views/training_resources/show.html.erb_spec.rb
+++ spec/views/training_resources/show.html.erb_spec.rb
@@ -1,11 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "training_resources/show", type: :view do
   before(:each) do
     assign(:training_resource, TrainingResource.create!(
       state: 2,
-      document: nil
-    ))
+      document: nil,
+    ),)
   end
 
   it "renders attributes in <p>" do
