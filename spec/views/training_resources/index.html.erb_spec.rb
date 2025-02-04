c92f3dd69e3f18ec756cc1b8a58beb0c7ff053cf
Hernan Maguina <hernan.maguina@gmail.com>
2025-02-04 17:38:32 +0100

    update views

--- spec/views/training_resources/index.html.erb_spec.rb
+++ spec/views/training_resources/index.html.erb_spec.rb
@@ -3,13 +3,13 @@ require "rails_helper"
 RSpec.describe "training_resources/index", type: :view do
   before(:each) do
     assign(:training_resources, [
-      create(:training_resource),
-      create(:training_resource),
-    ],)
+      create(:training_resource, state: 1),
+      create(:training_resource, state: 2)
+    ])
   end
 
   it "renders a list of training_resources" do
-    cell_selector = "div#training_resources div"
+    cell_selector = "table>tbody>tr"
     render
     assert_select cell_selector, text: Regexp.new(/State:/), count: 2
   end
