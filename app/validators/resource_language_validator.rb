2462477942ec2a1cb373f296d1f24811d10d5a5e
Aleksei Petrov <hydrognomik@gmail.com>
2025-02-05 16:55:19 +0100

    Prevent duplicate file names within the same language scope.

--- app/validators/resource_language_validator.rb
+++ app/validators/resource_language_validator.rb
@@ -1,11 +1,13 @@
 class ResourceLanguageValidator < ActiveModel::Validator
   def validate(record)
+    return if record.file_name_override.blank?
+
     if TrainingResource.joins(:topic)
-                    .where(
-                      training_resources: { file_name_override: record.file_name_override },
-                      topics: { language_id: record.topic.language_id },
-                    )
-      .any?
+                       .where(
+                         training_resources: { file_name_override: record.file_name_override },
+                         topics: { language_id: record.topic.language_id },
+                       ).where.not(id: record.id)
+                       .any?
       record.errors.add(:file_name_override, "This filename is already in use")
     end
   end
--- spec/models/training_resource_spec.rb
+++ spec/models/training_resource_spec.rb
@@ -1,6 +1,20 @@
 require "rails_helper"
 
 RSpec.describe TrainingResource, type: :model do
+  subject { create(:training_resource) }
   it { should validate_presence_of(:state) }
   it { should have_one_attached(:document) }
+
+  it "should reject creation of resources with duplicated filenames within the same language" do
+    resource = create(:training_resource, file_name_override: "filename.pdf")
+    expect {
+      create(:training_resource, file_name_override: "filename.pdf", topic_id: resource.topic_id)
+    }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "should allow creation of resources without file_name_override" do
+    expect {
+      create(:training_resource, file_name_override: nil)
+    }.to change(TrainingResource, :count).by(1)
+  end
 end
