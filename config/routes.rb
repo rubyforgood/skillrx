57e0d35853f56335d14528ab4366366a20058529
dmitrytrager <dmitry.trager@gmail.com>
2025-02-08 22:57:39 +0100

    add sign out link for home page navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  root "home#index"
-  get "home/index", as: :home
-  get "dashboard/index", as: :dashboard
   resources :languages, only: %i[index show new create edit update]
   resources :passwords, param: :token
   resources :providers
@@ -21,8 +18,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "home/index", as: :home
+  get "dashboard/index", as: :dashboard
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   # Defines the root path route ("/")
+
+  root "home#index"
 end
