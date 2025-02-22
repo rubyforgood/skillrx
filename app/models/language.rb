d0a81d855ed185b4965fb6f9fd3e7bd9da421459
Hernan Maguina <hernan.maguina@gmail.com>
2025-02-22 14:15:19 +0100

    fix: better spec descriptions

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,7 +2,7 @@ class Language < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def file_storage_prefix
-    return "" if name == "English" || name.nil?
+    return "" if name.downcase == "english" || name.nil?
 
     "#{name.first(2).upcase}_"
   end
