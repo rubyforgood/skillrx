6ad6f7f5392cde79b07611aabfec549288ebc706
Danny Collier <294724+dcollie2@users.noreply.github.com>
2025-02-22 16:02:40 -0500

    Update language.rb

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,6 +1,6 @@
 class Language < ApplicationRecord
   has_many :topics, dependent: :destroy
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2 }
 
   def file_storage_prefix
     return "" if name.downcase == "english" || name.nil?
