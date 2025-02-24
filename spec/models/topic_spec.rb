6971077963fa0200449f3b79e9083df72c5c458d
Dalyl F <dalylf97@gmail.com>
2025-02-24 19:34:16 +0100

    add documents to topics

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,33 +3,34 @@
 # Table name: topics
 #
 #  id          :bigint           not null, primary key
-#  description :text             not null
+#  description :text
 #  state       :integer          default("active"), not null
 #  title       :string           not null
 #  uid         :uuid             not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  language_id :bigint
+#  old_id      :integer
 #  provider_id :bigint
 #
 # Indexes
 #
 #  index_topics_on_language_id  (language_id)
+#  index_topics_on_old_id       (old_id) UNIQUE
 #  index_topics_on_provider_id  (provider_id)
 #
-class Topic < ApplicationRecord
-  include Searcheable
+require "rails_helper"
 
-  belongs_to :language
-  belongs_to :provider
-  has_many :training_resources
-  has_many_attached :documents
+RSpec.describe Topic, type: :model do
+  subject { create(:topic) }
+  
+  context "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:language_id) }
+    it { should validate_presence_of(:provider_id) }
+  end
 
-  validates :title, :language_id, :provider_id, presence: true
-
-  STATES = %i[active archived].freeze
-
-  enum :state, STATES.map.with_index.to_h
-
-  scope :active, -> { where(state: :active) }
+  context "associations" do
+    it { should have_many_attached(:documents) }
+  end
 end
