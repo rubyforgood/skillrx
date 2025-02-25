57f773a5fde5d01d02c09747aa2226badcbb33b9
dmitrytrager <dmitry.trager@gmail.com>
2025-02-25 14:17:10 +0100

    add new action to set current provider

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def current_provider
+    @current_provider ||= begin
+      Current.user.providers.find(cookies.signed[:current_provider_id])
+    rescue ActiveRecord::RecordNotFound
+      Current.user.providers.first
+    end
+  end
 end
