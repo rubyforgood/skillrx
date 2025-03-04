3b4a8bb4bab3341057a45825ed0e7d66bf828f3c
Hernan Maguina <hernan.maguina@gmail.com>
2025-03-04 18:34:07 +0100

    chore: add cognate_tags method

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,15 +6,24 @@ class Tag < ActsAsTaggableOn::Tag
   has_many :reverse_tag_cognates, class_name: "TagCognate", foreign_key: :cognate_id
   has_many :reverse_cognates, through: :reverse_tag_cognates, source: :tag
 
+  # Returns a unique list of all cognate tags, including both direct and reverse relationships
+  #
+  # @return [Array<Tag>] unique array of associated cognate tags
+  def cognates_tags
+    (cognates + reverse_cognates).uniq
+  end
+
+  # Returns a list of all cognate tag names
+  #
+  # @return [Array<String>] array of cognate tag names
   def cognates_list
     cognates.pluck(:name) + reverse_cognates.pluck(:name)
   end
 
-  def cognates_list=(cognates_list_str)
-    self.cognates = Tag.where(name: cognates_list_str)
-  end
-
+  # Returns tags that are available to be set as cognates
+  #
+  # @return [ActiveRecord::Relation] collection of Tag records excluding self and existing cognates
   def available_cognates
-    Tag.all - [ cognates, self ]
+    Tag.where.not(id: cognates.pluck(:id)).where.not(id: id)
   end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,10 +1,59 @@
 require "rails_helper"
 
 RSpec.describe Tag, type: :model do
+  subject { create(:tag) }
+
   describe "associations" do
     it { should have_many(:tag_cognates) }
     it { should have_many(:cognates).through(:tag_cognates) }
     it { should have_many(:reverse_tag_cognates).class_name("TagCognate") }
     it { should have_many(:reverse_cognates).through(:reverse_tag_cognates) }
+  end
+
+  describe ".cognates_tags" do
+    it "returns all tags that are cognates of the given tag" do
+      cognate_tag = create(:tag)
+      create(:tag_cognate, tag: subject, cognate: cognate_tag)
+
+      expect(subject.cognates_tags).to include(cognate_tag)
+    end
+
+    context "with reverse cognates" do
+      it "returns all tags that are cognates of the given tag" do
+        another_tag = create(:tag)
+        create(:tag_cognate, tag: another_tag, cognate: subject)
+
+        expect(subject.cognates_tags).to include(another_tag)
+      end
+    end
+  end
+
+  describe ".cognates_list" do
+    it "returns all tags that are cognates of the given tag" do
+      cognate_tag = create(:tag)
+      create(:tag_cognate, tag: subject, cognate: cognate_tag)
+
+      expect(subject.cognates_list).to include(cognate_tag.name)
+    end
+
+    context "with reverse cognates" do
+      it "returns all tags that are cognates of the given tag" do
+        another_tag = create(:tag)
+        create(:tag_cognate, tag: another_tag, cognate: subject)
+
+        expect(subject.cognates_list).to include(another_tag.name)
+      end
+    end
+  end
+
+  describe ".available_cognates" do
+    it "returns all tags that are not cognates of the given tag" do
+      non_cognate_tags = create_list(:tag, 2)
+      cognate_tag = create(:tag)
+      create(:tag_cognate, tag: subject, cognate: cognate_tag)
+
+      expect(subject.available_cognates).not_to include(cognate_tag)
+      expect(subject.available_cognates).to be_all { |tag| non_cognate_tags.include?(tag) }
+    end
   end
 end
