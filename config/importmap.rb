1a719bfb8818bbe8b995a50ec2b07dd6b78d203e
Hernan Maguina <hernan.maguina@gmail.com>
2025-03-05 21:08:59 +0100

    chore: add bootstrap tags npm package #24

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "@hotwired--stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-use" # @0.52.3
+pin "bootstrap5-tags" # @1.7.7
