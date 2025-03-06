467db825566b0a5ad1268d648c4316fe3122f7a2
Hernan Maguina <hernan.maguina@gmail.com>
2025-03-06 21:13:45 +0100

    chore: add tags controller #24

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   # Defines the root path route ("/")
 
+
+  resources :tags, only: %i[index]
+
   root "home#index"
 end
