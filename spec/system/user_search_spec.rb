344ac10d13a6c9edae36a9c646b121010fc803c2
Olivia Bonnal <oliviabonnal@gmail.com>
2025-03-16 17:10:13 +0000

    Add search functionality to users
    There used to be a search functionality in the users index page that was
    using Mazer's datatable stylesheet (PR#52), but it was then removed in
    PR#86 as we discussed doing it our way instead.
    
    This commit enables searching Users by using a similar logic to what
    Dmitry did for searching Topics. For the JS controller, I re-used what
    we had in topics_controller.js but did not replace topics_controller.js
    with the more generic "search_controller.js" after discussion with
    Dmitry during PR review. This is because the chooseProvider function is
    specific to the topics controller and replacing it with a generic name
    such as chooseOption may be too generic if we then need to add other
    choosing actions on Topics.

---
+++ spec/system/user_search_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.describe "User search", type: :system do
+  let(:admin) { create(:user, :admin, email: "admin@mail.com", created_at: 3.days.ago) }
+  let!(:martin) { create(:user, email: "martin@mail.com", created_at: 2.days.ago) }
+  let!(:rosemary) { create(:user, email: "rosemary@mail.com", created_at: 1.day.ago) }
+
+  before do
+    login_as(admin)
+    click_link("Users")
+  end
+
+  it "shows by default the users from most to least recently added" do
+    expect(page).to have_text(/#{rosemary.email}.+#{martin.email}.+#{admin.email}/m)
+  end
+
+  context "when searching by email" do
+    it "only displays users matching the search" do
+      fill_in "search[email]", with: "mar"
+
+      expect(page).to have_text(rosemary.email)
+      expect(page).to have_text(martin.email)
+      expect(page).not_to have_text(admin.email)
+    end
+  end
+
+  context "when searching by role" do
+    it "only displays users matching the search" do
+      select "Admin", from: "search_is_admin"
+
+      expect(page).to have_text(admin.email)
+      expect(page).not_to have_text(rosemary.email)
+      expect(page).not_to have_text(martin.email)
+
+      select "Contributor", from: "search_is_admin"
+
+      expect(page).to have_text(rosemary.email)
+      expect(page).to have_text(martin.email)
+      expect(page).not_to have_text(admin.email)
+    end
+  end
+
+  context "when sorting" do
+    it "displays users in the selected order" do
+      select "By least recently added", from: "search_order"
+      expect(page).to have_text(/#{admin.email}.+#{martin.email}.+#{rosemary.email}/m)
+    end
+  end
+end
