2d7884863b2fdc32c72e6db01422bda4ea19c11d
Olivia Bonnal <oliviabonnal@gmail.com>
2025-03-26 19:50:36 +0000

    Remove button to upload files
    We had originally added a button like in the article we followed, but
    felt that it would be more user-friendly to automatically upload the
    file once it is selected.
    
    For some reason the test for editing a topic in upload_management_spec.rb
    was failing on my machine because it either couldn't see the Topic or
    was on the Dashboard instead of being on the "Topics" page. I'm not sure
    why this was an issue since it was working on Dalyl's computer and the CI,
    but moving "click_link("Topics")" to inside the "it" blocks solved the
    issue.

--- spec/system/upload_management_spec.rb
+++ spec/system/upload_management_spec.rb
@@ -3,39 +3,35 @@ require "rails_helper"
 RSpec.describe "Upload Management", type: :system do
   let(:admin) { create(:user, :admin, email: "admin@mail.com") }
 
-
   before do
     login_as(admin)
-    click_link("Topics")
   end
-
 
   context "When create new Topic" do
     it "show added documents" do
+      click_link("Topics")
       click_link("Add New Topic")
         page.attach_file(Rails.root.join("spec/support/images/logo_ruby_for_good.png")) do
         page.find("#documents").click
       end
-      click_button("upload")
       expect(page).to have_text("logo_ruby_for_good.png")
     end
 
-
     it "deletes added documents" do
+      click_link("Topics")
       click_link("Add New Topic")
       page.attach_file(Rails.root.join("spec/support/images/logo_ruby_for_good.png")) do
         page.find("#documents").click
       end
-      click_button("upload")
       expect(page).to have_text("logo_ruby_for_good.png")
       click_button("remove-button")
       expect(page).not_to have_text("logo_ruby_for_good.png")
     end
   end
 
-
   context "When update Topic " do
-    let!(:topic) { create(:topic) }
+    let(:topic) { create(:topic) }
+
     before do
       topic.documents.attach(
         io: File.open(Rails.root.join("spec/support/images/logo_ruby_for_good.png")),
@@ -43,13 +39,14 @@ RSpec.describe "Upload Management", type: :system do
         content_type: "image/png"
       )
     end
+
     it "Doesn't replace documents" do
+      click_link("Topics")
       click_link("Edit")
       expect(page).to have_text("logo_ruby_for_good.png")
       page.attach_file(Rails.root.join("spec/support/files/file_text_test.txt")) do
         page.find("#documents").click
       end
-      click_button("upload")
       expect(page).to have_text("logo_ruby_for_good.png")
       expect(page).to have_text("file_text_test.txt")
     end
