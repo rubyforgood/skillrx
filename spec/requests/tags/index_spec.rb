a782764925874f5c89743ab7b7c7b93755673768
Hernan Maguina <hernan.maguina@gmail.com>
2025-03-29 16:14:43 +0100

    chore(tags): add tag resource

---
+++ spec/requests/tags/index_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+describe "Tags", type: :request do
+  describe "GET /tags" do
+    let(:user) { create(:user) }
+
+    before do
+      sign_in(user)
+    end
+
+    it "renders a successful response" do
+      tag = create(:tag)
+
+      get tags_url
+
+      expect(response).to be_successful
+      expect(assigns(:tags)).to eq([ tag ])
+    end
+  end
+end
