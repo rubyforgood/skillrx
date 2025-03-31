341c8886af9572f10dc3bc16f32ef9f8aecc6f68
Olivia Bonnal <oliviabonnal@gmail.com>
2025-03-31 19:11:20 +0100

    Tidy up upload_management_spec.rb

--- spec/system/upload_management_spec.rb
+++ spec/system/upload_management_spec.rb
@@ -7,19 +7,17 @@ RSpec.describe "Upload Management", type: :system do
     login_as(admin)
   end
 
-  context "When create new Topic" do
-    it "show added documents" do
-      click_link("Topics")
-      click_link("Add New Topic")
-        page.attach_file(Rails.root.join("spec/support/images/logo_ruby_for_good.png")) do
+  context "when creating a new topic" do
+    before { visit_with_wait(new_topic_path) }
+
+    it "shows added documents" do
+      page.attach_file(Rails.root.join("spec/support/images/logo_ruby_for_good.png")) do
         page.find("#documents").click
       end
       expect(page).to have_text("logo_ruby_for_good.png")
     end
 
     it "deletes added documents" do
-      click_link("Topics")
-      click_link("Add New Topic")
       page.attach_file(Rails.root.join("spec/support/images/logo_ruby_for_good.png")) do
         page.find("#documents").click
       end
@@ -29,7 +27,7 @@ RSpec.describe "Upload Management", type: :system do
     end
   end
 
-  context "When update Topic " do
+  context "when updating a topic " do
     let(:topic) { create(:topic) }
 
     before do
@@ -38,14 +36,13 @@ RSpec.describe "Upload Management", type: :system do
         filename: "logo_ruby_for_good.png",
         content_type: "image/png"
       )
+      visit edit_topic_path(topic)
     end
 
     context "when adding a document" do
-      it "Doesn't replace pre-existing documents" do
-        click_link("Topics")
-        click_link("Edit")
+      it "doesn't replace pre-existing documents" do
         expect(page).to have_text("logo_ruby_for_good.png")
-        page.attach_file(Rails.root.join("spec/support/files/file_text_test.txt")) do
+        page.attach_file(Rails.root.join("test/fixtures/files/file_text_test.txt")) do
           page.find("#documents").click
         end
         expect(page).to have_text("logo_ruby_for_good.png")
@@ -55,8 +52,6 @@ RSpec.describe "Upload Management", type: :system do
 
     context "when removing a document" do
       it "removes the document" do
-        click_link("Topics")
-        click_link("Edit")
         expect(page).to have_text("logo_ruby_for_good.png")
         click_button("remove-button-1")
         expect(page).not_to have_text("logo_ruby_for_good.png")
