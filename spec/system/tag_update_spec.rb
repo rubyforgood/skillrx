f3855b481946ea8609e8521cf6883c841e3688bd
Olivia Bonnal <oliviabonnal@gmail.com>
2025-06-02 12:34:03 +0100

    Fix issue with adding new cognates to Tags
    This resolves issue #177. Adding a new cognate to a Tag was failing
    silently and showing the form without displaying any errors. I saw in the
    logs that the problem happened because we tried to create a tag_cognate
    again for the Cognates that were already associated with the Tag, so I
    changed #create to #find_or_create_by.

--- spec/system/tag_update_spec.rb
+++ spec/system/tag_update_spec.rb
@@ -3,9 +3,11 @@ require "rails_helper"
 RSpec.describe "Updating a Tag", type: :system do
   let(:user) { create(:user, :admin) }
   let(:tag) { create(:tag, name: "Python") }
+  let(:cognate) { create(:tag, name: "Cython") }
 
   before do
     login_as(user)
+    create(:tag_cognate, tag: tag)
     wait_and_visit(edit_tag_path(tag.id))
   end
 
