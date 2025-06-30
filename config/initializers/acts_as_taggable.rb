9bd5a9ba98b0d9492fc4bb4543132430c56681eb
Olivia Bonnal <oliviabonnal@gmail.com>
2025-06-30 18:46:22 +0100

    Ensure the destroy callbacks on TagCognates work
    I saw that, if a Topic was tagged with a Tag and
    one of its Cognates, ActiveRecord::InvalidForeignKey
    was raised when removing and attempting to
    delete the Tag, because the dependent: :destroy
    callback wasn't executed. Moving the association
    to the parent class solves the issue.

--- config/initializers/acts_as_taggable.rb
+++ config/initializers/acts_as_taggable.rb
@@ -1,1 +1,7 @@
+ActsAsTaggableOn::Tag.class_eval do
+  has_many :tag_cognates, dependent: :destroy
+  # Reverse relationship for cognates referencing this tag
+  has_many :reverse_tag_cognates, class_name: "TagCognate", foreign_key: :cognate_id, dependent: :destroy
+end
+
 ActsAsTaggableOn.remove_unused_tags = true
