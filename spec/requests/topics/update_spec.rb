ae103fffda8c839f935ec02f6f787f9fd7464bc2
Olivia Bonnal <oliviabonnal@gmail.com>
2025-07-01 19:10:45 +0100

    Fix flaky spec/system/topics/update_spec.rb tests
    spec/system/topics/update_spec.rb:29 errors on the CI but not in local.
    I tried changing wait_and_visit topic_path(topic) to
    find_link("View", href: topic_path(topic)).click, but that also errored
    only in the CI, so I ended up rewriting these tests as request tests.

--- spec/requests/topics/update_spec.rb
+++ spec/requests/topics/update_spec.rb
@@ -1,18 +1,68 @@
 require "rails_helper"
 
-describe "Topics", type: :request do
+RSpec.describe "Topics", type: :request do
+  let(:user) { create(:user) }
+  let(:provider) { create(:provider) }
+  let(:language) { create(:language) }
+  let!(:topic) { create(:topic, provider: provider, language: language) }
+
+  before { sign_in(user) }
+
   describe "PUT /topics/:id" do
-    let(:user) { create(:user) }
-    let(:topic) { create(:topic) }
+    context "when removing a tag" do
+      let!(:tag) { create(:tag, name: "Tag to remove") }
 
-    before { sign_in(user) }
+      before do
+        topic.set_tag_list_on(topic.language.code.to_sym, tag.name)
+        topic.save
+      end
 
-    it "updates the Topic" do
-      topic_params = { title: "new topic", description: "updated" }
+      context "when the tag is not used on any other topic" do
+        let(:topic_params) {  { tag_list: [ "" ] } }
 
-      expect(put topic_url(topic), params: { topic: topic_params }).to redirect_to(topics_url)
-      expect(topic.reload.title).to eq("new topic")
-      expect(topic.description).to eq("updated")
+        it "removes the tag from the topic and destroys the unused tag" do
+          put topic_url(topic), params: { topic: topic_params }
+          expect(response).to redirect_to(topics_url)
+          expect(topic.reload.current_tags).to be_empty
+          expect(Tag.find_by(name: tag.name)).to be_nil
+        end
+      end
+
+      context "when the tag is still used on another topic" do
+        let!(:topic_2) { create(:topic, provider: provider, language: language) }
+        let(:topic_params) {  { tag_list: [ "" ] } }
+
+        before do
+          topic_2.set_tag_list_on(topic.language.code.to_sym, tag.name)
+          topic_2.save
+        end
+
+        it "removes the tag from the topic but does not destroy the tag" do
+          put topic_url(topic), params: { topic: topic_params }
+          expect(response).to redirect_to(topics_url)
+          expect(topic.reload.current_tags).to be_empty
+          expect(Tag.find_by(name: tag.name)).to be_present
+        end
+      end
+    end
+
+    context "when removing a tag whose cognate remains associated to the Topic" do
+      let!(:tag) { create(:tag, name: "Tag to remove") }
+      let!(:cognate) { create(:tag, name: "Cognate to keep") }
+      let(:topic_params) {  { tag_list: [ cognate.name ] } }
+
+      before do
+        tag.cognates << cognate
+        topic.set_tag_list_on(topic.language.code.to_sym, "#{tag.name},#{cognate.name}")
+        topic.save
+      end
+
+      it "removes the tag but keeps the cognate" do
+        put topic_url(topic), params: { topic: topic_params }
+        expect(response).to redirect_to(topics_url)
+        expect(topic.reload.current_tags).to eq([ cognate ])
+        expect(Tag.find_by(name: tag.name)).to be_nil
+      end
     end
   end
 end
