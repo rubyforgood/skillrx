ca62c3a334f1c0de5c099deab1c0c8abe9a51876
dmitrytrager <dmitry.trager@gmail.com>
2025-07-13 17:24:06 +0200

    log errors from jobs

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,4 +4,14 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
+
+  around_perform do |job, block|
+    Rails.logger.debug "[Job Start] #{job.class.name} ID=#{job.job_id}"
+    block.call
+    Rails.logger.debug "[Job Finish] #{job.class.name} ID=#{job.job_id}"
+  rescue => e
+    Rails.logger.error "[Job Error] #{job.class.name} ID=#{job.job_id} - #{e.class}: #{e.message}"
+    Rails.logger.error e.backtrace.join("\n")
+    raise
+  end
 end
