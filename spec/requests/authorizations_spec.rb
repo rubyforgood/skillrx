cbb92b55d586e267cae24c2b10088f8175744139
Olivia Bonnal <oliviabonnal@gmail.com>
2025-07-28 13:49:26 +0100

    Ensure only Admins can access Tag actions

--- spec/requests/authorizations_spec.rb
+++ spec/requests/authorizations_spec.rb
@@ -26,6 +26,31 @@ RSpec.describe "Authorizations", type: :request do
       expect(response).to redirect_to(topics_path)
     end
 
+    context "Tag-related actions" do
+      let!(:tag) { create(:tag) }
+
+      it "cannot access the Tags index" do
+        expect(get "/tags").to redirect_to(topics_path)
+      end
+
+      it "cannot access Tag show page" do
+        expect(get "/tags/#{tag.id}").to redirect_to(topics_path)
+      end
+
+      it "cannot access Tag edit page" do
+        expect(get "/tags/#{tag.id}/edit").to redirect_to(topics_path)
+      end
+
+      it "cannot make a Tag update request" do
+        expect { put "/tags/#{tag.id}", params: { tag: { name: "Updated Tag" } } }.not_to change { tag.reload.updated_at }
+        expect(response).to redirect_to(topics_path)
+      end
+
+      it "cannot make a Tag delete request" do
+        expect(delete "/tags/#{tag.id}").to redirect_to(topics_path)
+      end
+    end
+
     it "cannot access the Users tab" do
       get "/users"
       expect(response).to redirect_to(topics_path)
@@ -40,22 +65,22 @@ RSpec.describe "Authorizations", type: :request do
       expect(response).to be_successful
     end
 
-    it "cannot access the Regions tab" do
+    it "can access the Regions tab" do
       get "/regions"
       expect(response).to be_successful
     end
 
-    it "cannot access the Providers tab" do
+    it "can access the Providers tab" do
       get "/providers"
       expect(response).to be_successful
     end
 
-    it "cannot access the Languages tab" do
+    it "can access the Languages tab" do
       get "/languages"
       expect(response).to be_successful
     end
 
-    it "cannot access the Users tab" do
+    it "can access the Users tab" do
       get "/users"
       expect(response).to be_successful
     end
