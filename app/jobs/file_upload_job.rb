6ab809f0fd8a484c71b86f64471c98bb2e48288c
Danny Collier <294724+dcollie2@users.noreply.github.com>
2025-07-29 18:11:22 -0400

    Limit file upload job concurrency

--- app/jobs/file_upload_job.rb
+++ app/jobs/file_upload_job.rb
@@ -1,4 +1,6 @@
 class FileUploadJob < ApplicationJob
+  limits_concurrency  to: 1, key: ->(language_id) { language_id }, duration: 5.minutes
+
   def perform(language_id, file_id = nil, provider_id = nil, share = ENV["AZURE_STORAGE_SHARE_NAME"])
     @language = Language.find(language_id)
     @processor = LanguageContentProcessor.new(language)
