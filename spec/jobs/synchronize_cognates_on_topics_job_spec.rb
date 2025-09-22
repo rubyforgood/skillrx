e4717e6d4f42ef0d8a6e315711c7488a55d00a52
Olivia Bonnal <oliviabonnal@gmail.com>
2025-09-22 20:58:37 +0100

    Remove language context from SynchronizeCognatesOnTopicsJob

--- spec/jobs/synchronize_cognates_on_topics_job_spec.rb
+++ spec/jobs/synchronize_cognates_on_topics_job_spec.rb
@@ -13,15 +13,15 @@ RSpec.describe SynchronizeCognatesOnTopicsJob, type: :job do
   let!(:spanish_reverse_cognate) { create(:tag, name: "spanish reverse cognate") }
 
   before do
-    spanish_topic_1.set_tag_list_on(:sp, "spanish cognate,spanish reverse cognate")
+    spanish_topic_1.tag_list.add(["spanish cognate", "spanish reverse cognate"])
     spanish_topic_1.save
-    english_topic_2.set_tag_list_on(:en, "english cognate,english reverse cognate")
+    english_topic_2.tag_list.add(["english cognate", "english reverse cognate"])
     english_topic_2.save
   end
 
   context "when adding cognates to a tag" do
     before do
-      english_topic_1.set_tag_list_on(:en, "tag")
+      english_topic_1.tag_list.add(["tag"])
       english_topic_1.save
       create(:tag_cognate, tag: tag, cognate: english_cognate)
       create(:tag_cognate, tag: english_reverse_cognate, cognate: tag)
@@ -34,7 +34,7 @@ RSpec.describe SynchronizeCognatesOnTopicsJob, type: :job do
     it "adds new cognates to topics tagged with the original tag" do
       SynchronizeCognatesOnTopicsJob.perform_now(tag)
       expect(Topic.find_by(id: english_topic_1.id).current_tags_list)
-        .to match_array([ "tag", "english cognate", "english reverse cognate" ])
+        .to match_array([ "tag", "english cognate", "english reverse cognate", "spanish cognate", "spanish reverse cognate" ])
     end
   end
 end
