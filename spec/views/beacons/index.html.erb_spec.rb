639cbe1f0ed90370912eab9df07851bf5744e44f
David Gero <davidpgero@gmail.com>
2026-05-25 12:31:18 +0200

    634 update after review

--- spec/views/beacons/index.html.erb_spec.rb
+++ spec/views/beacons/index.html.erb_spec.rb
@@ -4,15 +4,14 @@ RSpec.describe "beacons/index", type: :view do
   context "when there are no beacons" do
     before do
       assign(:beacons, [])
+      render
     end
 
     it "renders an empty state message" do
-      render
       assert_select "td", text: /No beacons provisioned yet/
     end
 
     it "renders the provision new beacon link" do
-      render
       assert_select "a", text: /Provision New Beacon/
     end
   end
@@ -23,46 +22,39 @@ RSpec.describe "beacons/index", type: :view do
 
     before do
       assign(:beacons, [active_beacon, revoked_beacon])
+      render
     end
 
     it "renders a row for each beacon" do
-      render
       assert_select "table tbody tr", count: 2
     end
 
     it "shows Active status for active beacons" do
-      render
       assert_select "td span", text: "Active"
     end
 
     it "shows Revoked status for revoked beacons" do
-      render
       assert_select "td span", text: "Revoked"
     end
 
     it "renders each beacon name" do
-      render
       assert_select "td", text: /#{active_beacon.name}/
       assert_select "td", text: /#{revoked_beacon.name}/
     end
 
     it "shows the active count in the header" do
-      render
       assert_select "span.text-green-600", text: "1"
     end
 
     it "shows the revoked count in the header" do
-      render
       assert_select "span.text-red-600", text: "1"
     end
 
     it "shows the total beacon count in the summary" do
-      render
       assert_select "p.text-3xl", text: "2"
     end
 
     it "renders the Status column header without text wrapping" do
-      render
       assert_select "th.whitespace-nowrap", text: /Status/
     end
   end
